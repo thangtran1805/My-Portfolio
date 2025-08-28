51121e93adba8cd6c51164f16b5a39984e339aee
thangtran1805 <thangtranquoc1805@gmail.com>
2025-08-28 23:14:18 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Data Engineer', 'YouTuber', 'Blogger'],
+    strings: ['Data Engineer', 'Data Analyst', 'Teacher'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
